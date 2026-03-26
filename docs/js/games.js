48aeb9e328f5df2536202d73ea92446f678cb69d
Kepka Ludovic <shionn@gmail.com>
2026-03-26 22:16:52 +0100

    Debut de liste

--- docs/js/games.js
+++ docs/js/games.js
@@ -2,15 +2,14 @@
 
 
 class Game {
-	
+
 	constructor(name) {
 		this.name = name;
 		this.tags = [];
 		this.dlcs = [];
 	}
-	
+
 	tag(tags) {
-		this
 		this.tags = this.tags.concat(tags);
 		return this;
 	}
@@ -19,14 +18,36 @@ class Game {
 		this.dlcs.push(dlc);
 		return this;
 	}
+
+	fullname(fullname) {
+		this.fullname =fullname;
+		return this;
+	}
+
+	get img() {
+		let img = this.name.toLowerCase();
+		return "img/" + img.charAt(0) + "/" + img + ".jpg";
+	}
 }
 
 var GAMES = [
-	new Game("BioShock").tag("Shooter","FPP","Sci-fi"),
-	new Game("Timberborn").tag("Simulation","Building","Survival")
+	new Game("BioShock").tag("Shooter", "FPP", "Sci-fi"),
+	new Game("Brigador").fullname("Brigador: Up-Armored Edition").tag("Action","Combat","Tactique"),
+	new Game("Cyberpunk 2077").tag("Jeu de rôle","Action","SF"),
+	new Game("D").fullname("D: Résoudre le Mystére... Explorer le Côté Noir de Votre âme").tag("Horreur", "Aventure", "Réflexion"),
+	new Game("Darksiders").tag("Action", "Fantasy", "Jeu de rôle"),
+	new Game("Darksiders 2").tag("Action", "Fantasy", "Jeu de rôle"),
+	new Game("Darksiders 3").tag("Action", "Aventure", "Fantasy"),
+	new Game("Everspace").tag("Action", "Simulation", "SF"),
+	new Game("Fantasy General").tag("Stratégie", "Tour par tour", "Fantasy"),
+	new Game("Timberborn").tag("Simulation", "Building", "Survival")
 ]
 
 q(function() {
-		
+	GAMES.forEach((game) => {
+		let div = q("<div>");
+		div.append(q("<img>").src(game.img)).append(q("<span>").text(game.name));
+		q("#games").append(div);
+	});
 });
 
