65cc2b6ebc831fa313f378a29be460fd5ac255ab
Shionn <shionn@gmail.com>
2026-03-27 14:01:06 +0100

    un peu de style

--- docs/js/games.js
+++ docs/js/games.js
@@ -5,12 +5,18 @@ class Game {
 
 	constructor(name) {
 		this.name = name;
+		this.genres = [];
 		this.tags = [];
 		this.dlcs = [];
 	}
 
 	tag(tags) {
 		this.tags = this.tags.concat(tags);
+		return this;
+	}
+	
+	genre(genres) {
+		this.genres = this.genres.concat(genres);
 		return this;
 	}
 
@@ -31,23 +37,23 @@ class Game {
 }
 
 var GAMES = [
-	new Game("BioShock").tag("Shooter", "FPP", "Sci-fi"),
-	new Game("Brigador").fullname("Brigador: Up-Armored Edition").tag("Action","Combat","Tactique"),
-	new Game("Cyberpunk 2077").tag("Jeu de rôle","Action","SF"),
-	new Game("D").fullname("D: Résoudre le Mystére... Explorer le Côté Noir de Votre âme").tag("Horreur", "Aventure", "Réflexion"),
-	new Game("Darksiders").tag("Action", "Fantasy", "Jeu de rôle"),
-	new Game("Darksiders 2").tag("Action", "Fantasy", "Jeu de rôle"),
-	new Game("Darksiders 3").tag("Action", "Aventure", "Fantasy"),
-	new Game("Everspace").tag("Action", "Simulation", "SF"),
-	new Game("Fantasy General").tag("Stratégie", "Tour par tour", "Fantasy"),
-	new Game("Ori and the Blind Forest").tag("Action", "Aventure", "Plateforme"),
-	new Game("The Witness").tag("Aventure", "Réflexion", "Monde ouvert"),
-	new Game("Timberborn").tag("Simulation", "Building", "Survival"),
+	new Game("BioShock").genre("Shooter", "FPP", "Sci-fi"),
+	new Game("Brigador").fullname("Brigador: Up-Armored Edition").genre("Action","Combat","Tactique"),
+	new Game("Cyberpunk 2077").genre("Jeu de rôle","Action","SF"),
+	new Game("D").fullname("D: Résoudre le Mystére... Explorer le Côté Noir de Votre âme").genre("Horreur", "Aventure", "Réflexion"),
+	new Game("Darksiders").genre("Action", "Fantasy", "Jeu de rôle"),
+	new Game("Darksiders 2").genre("Action", "Fantasy", "Jeu de rôle"),
+	new Game("Darksiders 3").genre("Action", "Aventure", "Fantasy"),
+	new Game("Everspace").genre("Action", "Simulation", "SF"),
+	new Game("Fantasy General").genre("Stratégie", "Tour par tour", "Fantasy"),
+	new Game("Ori and the Blind Forest").genre("Action", "Aventure", "Plateforme"),
+	new Game("The Witness").genre("Aventure", "Réflexion", "Monde ouvert"),
+	new Game("Timberborn").genre("Simulation", "Building", "Survival"),
 ]
 
 q(function() {
 	GAMES.forEach((game) => {
-		let div = q("<div>");
+		let div = q("<div>").attr("data-game", game.name);
 		div.append(q("<img>").src(game.img)).append(q("<span>").text(game.name));
 		q("#games").append(div);
 	});
