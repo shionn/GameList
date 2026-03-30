1ed858c536845e668c80ae8d8a95c9c340fa0a2f
Kepka Ludovic <shionn@gmail.com>
2026-03-30 16:28:21 +0200

    Ajout

--- docs/js/games.js
+++ docs/js/games.js
@@ -9,6 +9,7 @@ class Game {
 		this.tags = [];
 		this.dlcs = [];
 		this.collection = undefined;
+		this.disk = false;
 	}
 
 	tag(...tags) {
@@ -58,6 +59,10 @@ class Game {
 		return this.fullname || this.name;
 	}
 
+	onDisk() {
+		this.disk = true;
+		return this;
+	}
 
 }
 
@@ -124,13 +129,13 @@ var GAMES = [
 		.genre("Simulation", "Historique", "Stratégie")
 		.tag("Stratégie", "Classique", "Simulation", "Superbe bande-son", "Gestion", "Bac-à-sable", "Historique", "Temps réél", "Gestion des resources", "Relaxant", "Médieval", "Construction de villes", "Commerce", "Transport"),
 
-	new Game("Beyond Good & Evil")
+	new Game("Beyond Good & Evil").onDisk()
 		.genre("Action", "TPP", "SF")
 		.tag("Action", "Histoire Riche", "Atmosphère", "Science fiction", "Classique", "Exploration", "Science", "Protagoniste féminine", "Superbe bande-son", "Monde Ouvert", "Troisième personne", "Plateformes", "Émotionnel", "Infiltration", "Dystopique"),
-	new Game("BioShock").setFullname("BioShock & BioShock Remastered")
+	new Game("BioShock").setFullname("BioShock & BioShock Remastered").onDisk()
 		.genre("Shooter", "FPP", "Sci-fi")
 		.tag("Histoire Riche", "Atmosphère", "Science fiction", "Classique", "Science", "Première personne", "Sombre", "Horreur", "FPS", "Tir", "Dystopique", "Steampunk", "Sous l'eau"),
-	new Game("Brigador").setFullname("Brigador: Up-Armored Edition")
+	new Game("Brigador").setFullname("Brigador: Up-Armored Edition").onDisk()
 		.genre("Action", "Combat", "Tactique")
 		.tag("Action", "Indé", "Science", "Tactique", "Difficile", "Isométrique", "Vue du dessus", "Roguelite", "Combat", "Cyberpunk", "Dystopique", "Tir à deux joysticks", "Chars d'assaut"),
 
@@ -138,7 +143,7 @@ var GAMES = [
 		.genre("Jeu de rôle", "Action", "SF")
 		.tag("Action", "Histoire Riche", "Atmosphère", "Jeu de rôle", "Science fiction", "Science", "Première personne", "Superbe bande-son", "Choix multiples", "Monde Ouvert", "Mature", "Nudité", "Violent", "Fins multiples", "FPS", "Cyberpunk"),
 
-	new Game("D").setFullname("D: Résoudre le Mystére... Explorer le Côté Noir de Votre âme")
+	new Game("D").setFullname("D: Résoudre le Mystére... Explorer le Côté Noir de Votre âme").onDisk()
 		.genre("Horreur", "Aventure", "Réflexion")
 		.tag("Aventure", "Atmosphère", "Classique", "Casse-tête", "Première personne", "Protagoniste féminine", "Sombre", "Horreur", "Fins multiples", "Logique", "Horreur psychologique", "FMV"),
 	new Game("Darksiders").setFullname("Darksiders Warmastered Edition").setCollection("Darksiders")
@@ -147,7 +152,7 @@ var GAMES = [
 	new Game("Darksiders 2").setCollection("Darksiders")
 		.genre("Action", "Fantasy", "Jeu de rôle")
 		.tag("Action", "Fantasy", "Histoire Riche", "Jeu de rôle", "Casse-tête", "Superbe bande-son", "Monde Ouvert", "Troisième personne", "Multijoueur", "Gore", "Hack and Slash", "Metroidvania"),
-	new Game("Darksiders 3").setCollection("Darksiders")
+	new Game("Darksiders 3").setCollection("Darksiders").onDisk()
 		.genre("Action", "Aventure", "Fantasy")
 		.tag("Aventure", "Action", "Fantasy", "Histoire Riche", "Atmosphère", "Casse-tête", "Protagoniste féminine", "Sombre", "Troisième personne", "Violent", "Gore", "Hack and Slash", "Post-apocalyptique", "Souls-like"),
 
@@ -155,14 +160,14 @@ var GAMES = [
 		.genre("Action", "Simulation", "SF")
 		.tag("Action", "Science fiction", "Classique", "Exploration", "Simulation", "Science", "Première personne", "Superbe bande-son", "Survie", "Difficile", "Roguelike", "Espace", "Roguelite", "Vol", "Réalité virtuelle"),
 
-	new Game("Fantasy General")
+	new Game("Fantasy General").onDisk()
 		.genre("Stratégie", "Tour par tour", "Fantasy")
 		.tag("Fantasy", "Stratégie", "2D", "Classique", "Tour par tour", "Magie", "Guerre"),
 	new Game("Full Throttle").setFullname("Full Throttle Remastered")
 		.genre("Aventure", "Point-and-click", "Enquête-mystère")
 		.tag("Aventure", "Histoire Riche", "Atmosphère", "Classique", "Superbe bande-son", "Graphismes Pixel", "Amusant", "Point&Click", "Enquête", "Enquête-mystère", "Court", "Remake"),
 
-	new Game("Grandia 2").setFullname("Grandia II Anniversary Edition").genre("Aventure", "RPG"),
+	new Game("Grandia 2").setFullname("Grandia II Anniversary Edition").onDisk().genre("Aventure", "RPG"),
 
 	new Game("Leisure Suit Larry 7").setFullname("Leisure Suit Larry : Drague en Haute Mer !")
 		.genre("Aventure", "Point-and-click", "Réflexion")
@@ -172,7 +177,7 @@ var GAMES = [
 		.genre("Jeu de rôle", "FPP", "Fantasy")
 		.tag("Fantasy", "Jeu de rôle", "Classique", "Exploration", "Tour par tour", "Première personne", "Gestion", "Monde Ouvert", "Magie", "FPS", "cRPG"),
 
-	new Game("Ori and the Blind Forest").setFullname("Ori and the Blind Forest: Definitive Edition")
+	new Game("Ori and the Blind Forest").setFullname("Ori and the Blind Forest: Definitive Edition").onDisk()
 		.genre("Action", "Aventure", "Plateforme")
 		.tag("Aventure", "Action", "Fantasy", "Histoire Riche", "Atmosphère", "2D", "Classique", "Casse-tête", "Superbe bande-son", "Monde Ouvert", "Plateformes", "Difficile", "Familial", "Metroidvania"),
 	new Game("Outcast").setFullname("Outcast - Second Contact")
@@ -212,10 +217,10 @@ var GAMES = [
 		.genre("Stratégie", "Temps réel", "Fantasy")
 		.tag("Fantasy", "Stratégie", "Classique", "Temps réél"),
 
-	new Game("The Elder Scrolls: Arena").setCollection("The Elder Scrolls")
+	new Game("The Elder Scrolls: Arena").setCollection("The Elder Scrolls").onDisk()
 		.genre("Jeu de rôle", "Action", "Monde ouvert")
 		.tag("Action", "Jeu de rôle", "Classique", "Monde Ouvert"),
-	new Game("The Elder Scrolls II: Daggerfall").setCollection("The Elder Scrolls")
+	new Game("The Elder Scrolls II: Daggerfall").setCollection("The Elder Scrolls").onDisk()
 		.genre("Jeu de rôle", "Aventure", " Monde ouvert")
 		.tag("Aventure", "Jeu de rôle", "Classique", "Monde Ouvert"),
 	new Game("The Elder Scrolls III: Morrowind").setFullname("The Elder Scrolls III: Morrowind - GOTY Edition").setCollection("The Elder Scrolls"),
@@ -226,19 +231,19 @@ var GAMES = [
 	new Game("The Witcher 3").setFullname("The Witcher 3: Wild Hunt - Complete Edition").setCollection("The Witcher")
 		.genre("Jeu de rôle", "Aventure", "Fantasy")
 		.tag("Aventure", "Fantasy", "Histoire Riche", "Atmosphère", "Jeu de rôle", "Classique", "Exploration", "Superbe bande-son", "Choix multiples", "Monde Ouvert", "Troisième personne", "Mature", "Contenu à caractère sexuel", "Nudité", "Violent", "Fins multiples", "Gore", "Magie", "Médieval", "Vampire", "Loups-garous"),
-	new Game("The Witness")
+	new Game("The Witness").onDisk()
 		.genre("Aventure", "Réflexion", "Monde ouvert")
 		.tag("Aventure", "Indé", "Histoire Riche", "Atmosphère", "Classique", "Exploration", "Casse-tête", "Casual", "Première personne", "Superbe bande-son", "Monde Ouvert", "Mystère", "Bac-à-sable", "Difficile", "Logique", "Relaxant", "Surréaliste", "Simulation de marche", "Cozy", "Philosophique"),
-	new Game("Theme Hospital")
+	new Game("Theme Hospital").onDisk()
 		.genre("Stratégie", "Construction", "Gestion")
 		.tag("Stratégie", "Classique", "Casual", "Amusant", "Gestion", "Construction", "Gestion", "Humour noir"),
 	new Game("Theme Park")
 		.genre("Simulation", "Construction", "Gestion")
 		.tag("Classique", "Simulation", "Construction", "Gestion"),
-	new Game("Timberborn")
+	new Game("Timberborn").onDisk()
 		.genre("Simulation", "Building", "Survival")
 		.tag("Science fiction", "Simulation", "Gestion", "Bac-à-sable", "Survie", "Gestion des resources", "Vue du dessus", "Construction", "Construction de base", "Post-apocalyptique", "Construction de villes", "Editeur de niveaux"),
-	new Game("Tomb Raider 1/2/3").setFullname("Tomb Raider I-III Remastered Starring Lara Croft").setCollection("Tomb Raider")
+	new Game("Tomb Raider 1/2/3").setFullname("Tomb Raider I-III Remastered Starring Lara Croft").setCollection("Tomb Raider").onDisk()
 		.genre("Action", "Aventure", "Exploration")
 		.tag("Aventure", "Action", "Atmosphère", "Classique", "Exploration", "Casse-tête", "Protagoniste féminine", "Violent", "Remake"),
 	new Game("Tomb Raider 4/5/6").setFullname("Tomb Raider IV-VI Remastered").setCollection("Tomb Raider")
@@ -378,7 +383,11 @@ q(function() {
 
 		let div = q("<div>");
 		div.append(q("<img>").src(game.img));
-		div.append(q("<h1>").text(game.displayName));
+		let h1 = q("<h1>").text(game.displayName);
+		if (game.disk) {
+			h1.append(q("<small>").text(ICON_DISK))
+		}
+		div.append(h1);
 
 		if (game.collection) {
 			let collection = q("<p>").append(q("<strong>").text("Collection : ")).append(q("<a>").href("#" + game.collection).data("collection", game.collection).text(game.collection));
